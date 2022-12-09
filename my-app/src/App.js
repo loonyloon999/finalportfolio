9f3d88f81f0e9ec16e1031db2bd9a96d6484989c
CalStell <74397392+CalStell@users.noreply.github.com>
2022-12-08 19:57:15 -0500

    Initial push

---
+++ my-app/src/App.js
@@ -0,0 +1,35 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <a
+          href="https://loonyloon999.github.io/ProductLandingPage/"
+          style={{ color: "green" }}
+          target="_blank"
+        >
+          Personas
+        </a>
+        <a
+          href="https://loonyloon999.github.io/ResponsiveRedesignPortfolio/"
+          style={{ color: "green" }}
+          target="_blank"
+        >
+          Responsive Redesign
+        </a>
+        <a
+          href="https://loonyloon999.github.io/development/"
+          style={{ color: "green" }}
+          target="_blank"
+        >
+          Development
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
